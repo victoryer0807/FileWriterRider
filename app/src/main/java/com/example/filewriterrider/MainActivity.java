91526b53f50980b73bfc99a82821c83f1976ed6c
VictorySamoletova <victoryer@mail.ru>
2023-03-25 01:13:33 +0300

    Commits how to work this App

--- app/src/main/java/com/example/filewriterrider/MainActivity.java
+++ app/src/main/java/com/example/filewriterrider/MainActivity.java
@@ -29,19 +29,20 @@ public class MainActivity extends AppCompatActivity {
        FileOutputStream fos = null;
 
        try {
-           EditText textBox = (EditText) findViewById(R.id.editor);
-           String text = textBox.getText().toString();
-           fos = openFileOutput(FILE_NAME, MODE_PRIVATE);
-           fos.write(text.getBytes());
+           EditText textBox = (EditText) findViewById(R.id.editor);//создаем текст
+           String text = textBox.getText().toString(); // nahodim etot text
+
+           fos = openFileOutput(FILE_NAME, MODE_PRIVATE);// nahodim file i stavim private tk files mogut bit ispolzovani tolko vnutri nashego prilozhenia i dostupni tolko vladelcu prilojenia
+           fos.write(text.getBytes());//sozdaem massiv baitov po razmeru string
            Toast.makeText(this, "Фаил успешно сохранен", Toast.LENGTH_LONG).show();
-       } catch (FileNotFoundException e){
+       } catch (FileNotFoundException e){ //obrabotka oshibok tsli fail ne naiden
            e.printStackTrace();
-       } catch (IOException e) {
+       } catch (IOException e) { // oshibka esli fail naiden no net dostupa na zapis'
            e.printStackTrace();
            Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
        }finally {
-           try {
-               if (fos != null) {
+           try {                 //proverka ne null li nash potok zapisey
+               if (fos != null) { // esli null to zakrit'
                    fos.close();
                }
            } catch (IOException e) {
@@ -52,15 +53,15 @@ public class MainActivity extends AppCompatActivity {
    }
 //открытие файла
     public void openText(View view){
-        FileInputStream fin = null;
-        TextView textView = (TextView) findViewById(R.id.text);
+        FileInputStream fin = null; // sozdaem input kak peremennuu
+        TextView textView = (TextView) findViewById(R.id.text); // nahodim pole dlya vivoda
 
         try {
             fin = openFileInput(FILE_NAME);
-            byte[] bytes = new byte[fin.available()];
-            fin.read(bytes);
-            String text = new String(bytes);
-            textView.setText(text);
+            byte[] bytes = new byte[fin.available()]; //massiv faila po razmeru dostupnih baytov
+            fin.read(bytes); //chitaem file v massiv baytov kuda zapisat tekst dlya sohraneniya
+            String text = new String(bytes); //peredaem v string
+            textView.setText(text); //vivodim na ekran
         } catch (FileNotFoundException e) {
             e.printStackTrace();
             Toast.makeText(this,e.getMessage(), Toast.LENGTH_SHORT).show();
