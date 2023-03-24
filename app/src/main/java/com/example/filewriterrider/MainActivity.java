50e3625cce24ac19e1445528bf6b4e6b47eed91b
VictorySamoletova <victoryer@mail.ru>
2023-03-24 17:15:41 +0300

    added try/catch

--- app/src/main/java/com/example/filewriterrider/MainActivity.java
+++ app/src/main/java/com/example/filewriterrider/MainActivity.java
@@ -3,6 +3,13 @@ package com.example.filewriterrider;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 public class MainActivity extends AppCompatActivity {
     private static String FILE_NAME = "content.txt";
@@ -14,6 +21,22 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+     //  сохранение файла
+   public void saveText(View view){
+       FileOutputStream fos = null;
+
+       try {
+           EditText textBox = (EditText) view.findViewById(R.id.editor);
+           String text = textBox.getText().toString();
+           fos = openFileOutput(FILE_NAME, MODE_PRIVATE);
+           fos.write(text.getBytes());
+       } catch (FileNotFoundException e){
+           e.printStackTrace();
+       } catch (IOException e) {
+           e.printStackTrace();
+       }
+
+   }
 
 
 
