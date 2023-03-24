3dd217c4581fe470b43e06f82f44fb9503e3b81a
VictorySamoletova <victoryer@mail.ru>
2023-03-24 16:00:11 +0300

    Implemented activity_main.xml

---
+++ app/src/main/java/com/example/filewriterrider/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.filewriterrider;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private static String FILE_NAME = "content.txt";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+
+
+}
